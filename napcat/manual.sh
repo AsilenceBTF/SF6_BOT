bc879f00c59ab175042b7c41bd0c0acba2c5705b
leitanglong <leitanglong@bytedance.com>
2025-10-02 20:19:11 +0800

    feat: add docker-compose.yml
    Change-Id: I34ca785b12ca8e02373d446271f050c991c87be7

---
+++ napcat/manual.sh
@@ -0,0 +1,16 @@
+# 查看状态
+sudo systemctl status docker
+# 查看docker容器
+docker ps -a
+# 登录docker容器
+docker exec -it id bash
+# 重启docker容器
+docker restart id
+# 查看容器内网IP
+docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' 容器名或ID
+# 查看当前容器的详细网络信息
+docker ps --format "table {{.Names}}\t{{.Ports}}\t{{.ID}}"
+# 查看所有镜像
+docker image ls
+# 构建历史
+docker history ID
